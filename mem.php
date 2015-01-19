93010b75a92038a5c453b1e7baa28fc47df37f20
Dafuk <github@dafuk.co.il>
2015-01-19 22:29:41 +0200

    mem.php is a helper for memcached, three stupid functions for lazy spaghetti-coder

---
+++ mem.php
@@ -0,0 +1,73 @@
+<?php  
+/*
+don't use this, I wrote it under influence
+
+*/
+
+class mem
+{
+    public $ram = false; 
+    public $entry_timeout = 3600; // 0 - infinite, 2592000 - maximum 
+    public $memhost = '127.0.0.1';
+    public $memport = 11211;
+    function __construct($memhost=false,$memport=false) 
+    {
+    	if ($memhost !== false)
+	{
+		$this->memhost=$memhost; // shorthand it when sober
+	}
+    	if ($memport !== false)
+	{
+		$this->memport=$memport; // shorthand it when sober
+	}
+
+
+	$this->make_this_shit_work(); 
+    }
+    function make_this_shit_work()
+    {
+	$memcache = new Memcache; //
+	$memcache->connect($this->memhost, $this->memport); // ever considered flexible configuration?
+	if(!is_object($memcache))
+	{
+	    die('insane in the membrane');
+	}
+	$this->ram = $memcache;
+    }
+    function save($key,$val) 
+    {
+	if(!is_object($this->ram)) 
+        {
+	    $this->make_this_shit_work(); 
+	}
+	if($key != 'count')
+	{
+	    $this->ram->set($key,$val, MEMCACHE_COMPRESSED, $this->entry_timeout); 
+	}
+	else
+	{
+	    $this->ram->set($key,$val);
+	}
+    }
+    function load($key)
+    {
+	if(!is_object($this->ram)) 
+        {
+	    $this->make_this_shit_work(); 
+	    return 0;
+	}
+	return $this->ram->get($key);
+    }
+    function increment($dest)
+    {
+	$o = $this->load($dest); 
+	if(empty($o))
+	{
+	    $o=0;
+	}
+	$this->save($dest,$o+1); 
+	return $o+1;
+    }
+    
+}
+?>
