e0e8c8e270e0abec28a1c39f9e46abd6ad6195c2
dafuk <ganjubaseg@gmail.com>
2015-07-07 20:23:19 +0300

    just sleepeee

--- benlib.sh
+++ benlib.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-function genpass()
+genpass()
 {
     cat /dev/urandom | head -n 1  | md5| head -c $1
 }
-function onlyroot()
+onlyroot()
 {
     if [ `id -u` != 0 ]; then
 	echo "This script must be run as root" 1>&2
 	exit 1
     fi
 }
-function sysusers()
+sysusers()
 {
     awk -F":" $'{ print $1 $3 }' /etc/passwd
 }
-function stripcomments()
+stripcomments()
 {
     grep -v '^[ \t]*$\|^[ \t]*#' $1
 }
